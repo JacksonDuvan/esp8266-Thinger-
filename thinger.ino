bdbea7a1a91547d31ea1ed6963b93bce16c22238
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-12-04 19:51:10 -0500

    Update thinger.ino

--- thinger.ino
+++ thinger.ino
@@ -1,17 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ThingerESP8266.h>
 
-#define USERNAME "your_user_name"
-#define DEVICE_ID "your_device_id"
-#define DEVICE_CREDENTIAL "your_device_credential"
+#define USERNAME "your_user_name" //Nombre de tu usuario 
+#define DEVICE_ID "your_device_id" //Nombre del dispositivo creado 
+#define DEVICE_CREDENTIAL "your_device_credential" //Credencial dada por Thinger  
 
-#define SSID "your_wifi_ssid"
-#define SSID_PASSWORD "your_wifi_ssid_password"
+#define SSID "your_wifi_ssid" //Nombre del router 
+#define SSID_PASSWORD "your_wifi_ssid_password" //Contraseña 
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);//Pin D1 esp8266
 
   thing.add_wifi(SSID, SSID_PASSWORD);
 
